1e660871e2897ceeed11904f139d079fdc483e58
brigugll <brigugll@bc.edu>
2023-04-20 13:14:14 -0400

    Initial Commit

---
+++ Final/FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalApp.swift
+//  Final
+//
+//  Created by Luke Briguglio on 4/20/23.
+//
+
+import SwiftUI
+
+@main
+struct FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
